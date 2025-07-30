f80c90730946cd84837ab0f70c4a3627036c1609
Minseok Choe <choems@outlook.com>
2025-07-30 22:50:54 +0900

    temp

--- src/main/scala/esmeta/verify/OrderGraph.scala
+++ src/main/scala/esmeta/verify/OrderGraph.scala
@@ -9,7 +9,7 @@ case class Inlined(invokeId: InvokeId, og: OrderGraph) extends Node
 
 class OrderGraph(
   val nodes: Set[Node],
-  val edges: Map[Node, Set[Node]]
+  val edges: Map[Node, Set[Node]],
 ) {
   // Define the structure of the order graph
   // This could include nodes, edges, and any other necessary components
--- src/main/scala/esmeta/verify/Transform.scala
+++ src/main/scala/esmeta/verify/Transform.scala
@@ -11,16 +11,36 @@ class Transform(entry: cfg.Func, builtCfg: cfg.CFG):
     val nodes = idMap.map[Node] { case (n, i) => CFGNode(i, n) }
     val edges: Map[Node, Set[Node]] = for {
       (from, to) <- entry.succs
-    } yield CFGNode(idMap(from), from) -> to.map[Node](n => CFGNode(idMap(n), n))
+    } yield CFGNode(idMap(from), from) -> to.map[Node](n =>
+      CFGNode(idMap(n), n),
+    )
     (nodes.toSet, edges)
 
   lazy val result: OrderGraph =
     val (nodes, edges) = wrapped(entry)
+
+    val checker = FindSdo(
+      builtCfg,
+      entry,
+    )
+
+    checker.analyze
+    val result = checker.getResult(
+      checker.NodePoint(
+        func = entry,
+        node = entry.nodes.head,
+        view = checker.View(),
+      ),
+    )
+
+    println(
+      s"Transform: ${entry.name} -> ${result}",
+    )
+
     new OrderGraph(nodes, edges)
 
 end Transform
 
-
 object Transform:
-  // def apply(): Transform = new Transform()
+// def apply(): Transform = new Transform()
 end Transform
