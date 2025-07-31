a5bdf2e03787f97a877d463df9bc888479e99dcd
Minseok Choe <choems@outlook.com>
2025-07-31 22:35:14 +0900

    format

--- src/main/scala/esmeta/verify/FindSdo.scala
+++ src/main/scala/esmeta/verify/FindSdo.scala
@@ -6,18 +6,18 @@ import esmeta.cfg.*
 class FindSdo(
   cfg: CFG,
   target: Func,
-  ) extends analyzer.tychecker.TyChecker(
-cfg,
-targetPattern = None,
-inferTypeGuard = true,
-typeSens = false,
-config = analyzer.tychecker.TyChecker.Config(),
-ignore = analyzer.tychecker.TyChecker.Ignore(),
-log = false,
-detail = false,
-silent = false,
-useRepl = false,
-replContinue = false,
-) {
+) extends analyzer.tychecker.TyChecker(
+    cfg,
+    targetPattern = None,
+    inferTypeGuard = true,
+    typeSens = false,
+    config = analyzer.tychecker.TyChecker.Config(),
+    ignore = analyzer.tychecker.TyChecker.Ignore(),
+    log = false,
+    detail = false,
+    silent = false,
+    useRepl = false,
+    replContinue = false,
+  ) {
   override def targetFuncs: List[Func] = List(target)
 }
